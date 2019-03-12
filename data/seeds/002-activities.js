4442bc822adb2320da232aa68741da65ad79b561
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-12 12:38:28 -0600

    fix pg truncate error (for now..)

--- data/seeds/002-activities.js
+++ data/seeds/002-activities.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries, reset ids
+  // Deletes ALL existing entries
   return knex("activities")
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("activities").insert([
