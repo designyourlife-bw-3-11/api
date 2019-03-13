4b903caec34e9721c2ff84af8091e7aba29da1af
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-13 07:30:30 -0600

    POST activity log

--- api/activity-logs/activity-logs-module.js
+++ api/activity-logs/activity-logs-module.js
@@ -2,7 +2,8 @@ const db = require("../../data/dbConfig.js");
 
 module.exports = {
   getAll,
-  getById
+  getById,
+  addActivityLog
 };
 
 async function getAll(username) {
@@ -20,5 +21,27 @@ async function getById(username, id) {
     return activityLog;
   } catch (error) {
     throw new Error("Could not fetch activity log.");
+  }
+}
+
+async function addActivityLog(activityData, activities) {
+  try {
+    const [id] = await db("activity-logs").insert(activityData, "id");
+    // const id = 3;
+    const activityLogActivities = activities.map(act => ({
+      activity_log_id: id,
+      activity_id: act.activity_id,
+      enjoyment: act.enjoyment,
+      engagement: act.engagement,
+      notes: act.notes
+    }));
+    // console.log(activityLogActivities);
+    const [id2] = await db("activity-log-activities").insert(
+      activityLogActivities,
+      "id"
+    );
+    return { id, id2 };
+  } catch (error) {
+    throw new Error(error);
   }
 }
--- api/activity-logs/activity-logs-router.js
+++ api/activity-logs/activity-logs-router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const ActivityLogs = require("./activity-logs-module.js");
-const ActivityLogActivities = require("../activity-log-activities/activity-log-activities-module.js");
+// const ActivityLogActivities = require("../activity-log-activities/activity-log-activities-module.js");
 
 router.get("/:user/:id?", async (req, res) => {
   const username = req.params.user;
@@ -26,6 +26,18 @@ router.get("/:user/:id?", async (req, res) => {
       });
       Promise.all(returnData).then(data => res.status(200).json(data));
     }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/:user", async (req, res) => {
+  const { user_id, date, outcomes, activities } = req.body;
+  const activityData = { user_id, date, outcomes };
+  // console.log(activityData, activities);
+  try {
+    const success = await ActivityLogs.addActivityLog(activityData, activities);
+    res.status(200).json({ "activity added: ": success });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
