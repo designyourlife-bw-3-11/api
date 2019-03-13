02990238588cb9dcf5fc9c63fe37176acfdcd0e3
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-12 18:36:20 -0600

    add functionality to post, put, delete activities

--- api/activities/activities-module.js
+++ api/activities/activities-module.js
@@ -2,7 +2,10 @@ const db = require("../../data/dbConfig.js");
 
 module.exports = {
   getAll,
-  getById
+  getById,
+  addActivity,
+  updateActivity,
+  deleteActivity
 };
 
 async function getAll(username) {
@@ -18,5 +21,52 @@ async function getById(username, id) {
   try {
     const activity = await db("activities").where({ id });
     return activity;
-  } catch (error) {}
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function addActivity(activityData) {
+  try {
+    if (activityData.name) {
+      const [id] = await db("activities").insert(activityData, "id");
+      return id;
+    } else {
+      throw "Please provide activity name.";
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateActivity(activityData) {
+  try {
+    const id = activityData.id;
+    if (id) {
+      const [updatedId] = await db("activities")
+        .where({ id })
+        .update({ ...activityData }, "id");
+      if (updatedId) {
+        return updatedId;
+      } else {
+        throw new Error("Error updating, is there an activity with that id?");
+      }
+    } else {
+      throw "Please provide activity id.";
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteActivity(delId) {
+  try {
+    const id = delId;
+    const deleted = await db("activities")
+      .where({ id })
+      .del();
+    return deleted;
+  } catch (error) {
+    throw error;
+  }
 }
--- api/activities/activities-router.js
+++ api/activities/activities-router.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Activities = require("../activities/activities-module.js");
+const User = require("../user/user-module.js");
 
 router.get("/:user/:id?", async (req, res) => {
   const username = req.params.user;
@@ -16,6 +17,62 @@ router.get("/:user/:id?", async (req, res) => {
       // return all activities
       const activities = await Activities.getAll(username);
       res.status(200).json(activities);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/:user", async (req, res) => {
+  let activityData = req.body;
+  const username = req.params.user;
+  try {
+    const user = await User.findBy({ username });
+    if (user) {
+      activityData.user_id = user.id;
+      const id = await Activities.addActivity(activityData);
+      res.status(200).json({ message: "Successfully added activity.", id });
+    } else {
+      res.status(400).json({ message: "Invalid username." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.put("/:user", async (req, res) => {
+  let activityData = req.body;
+  const username = req.params.user;
+  try {
+    const user = await User.findBy({ username });
+    if (user) {
+      activityData.user_id = user.id;
+      const id = await Activities.updateActivity(activityData);
+      res.status(200).json({ message: "Successfully updated activity.", id });
+    } else {
+      res.status(400).json({ message: "Invalid username." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/:user", async (req, res) => {
+  let delId = req.body.id;
+  const username = req.params.user;
+  try {
+    const user = await User.findBy({ username });
+    if (user) {
+      const deleted = await Activities.deleteActivity(delId);
+      if (deleted) {
+        res.status(200).json({ message: `Deleted ${deleted} activities.` });
+      } else {
+        res
+          .status(400)
+          .json({ message: "Provided id does not match any activities." });
+      }
+    } else {
+      res.status(400).json({ message: "Invalid username." });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
